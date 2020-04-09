1b5b3d1aa832d57146854eb837652afb863780a1
qiaoxinchun <1125542525@qq.com>
2020-04-09 22:29:43 +0800

    提交主干代码

--- src/test/java/com/qxc/hrms/HrmsApplicationTests.java
+++ src/test/java/com/qxc/hrms/HrmsApplicationTests.java
@@ -16,12 +16,8 @@ class HrmsApplicationTests {
 	void test1() {
         System.out.println("主干代码");
     }
-<<<<<<< HEAD
-=======
-
 
 	void test2() {
 		System.out.println("第二次提交主干代码");
 	}
->>>>>>> master
 }
